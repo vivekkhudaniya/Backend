ecb1ae6024463489dc589bae2cc485e723cfd01f
vivekkhudaniya <vivekkhudaniya@gmail.com>
2023-04-05 01:50:24 +0530

    Middle ware and api

--- indexExpress.js
+++ indexExpress.js
@@ -3,18 +3,38 @@ import path from "path";
 
 const app = express();
 
+const users = [];
+//using middleware
+app.use(express.static(path.join(path.resolve(),"public")));
+app.use(express.urlencoded({extended : true}));
+
+app.set("view engine","ejs");
 
 
-app.get("/getProducts",(req,res)=>{
-    //res.sendStatus(400) It helps to send the status
-    // res.json({
-    //     success:true,
-    //     products:[]
-    // })
-    const pathLocation = path.resolve();
-    res.sendFile(path.join(pathLocation,"./index.html"));
-    // res.status(400).send("Meri Marzi")
-})
+app.get("/",(req,res)=>{
+    res.render("index",{name : "Vivek"});
+});
+
+app.get("/success", (req,res) =>{
+    res.render("success");
+});
+
+app.post("/",(req,res) =>{
+    console.log(req.body);
+    users.push({username : req.body.name , email : req.body.email});
+    res.redirect("/success");
+});
+
+// app.get("/getProducts",(req,res)=>{
+//     //res.sendStatus(400) It helps to send the status
+//     // res.json({
+//     //     success:true,
+//     //     products:[]
+//     // })
+//     const pathLocation = path.resolve();
+//     res.sendFile(path.join(pathLocation,"./index.html"));
+//     // res.status(400).send("Meri Marzi")
+// })
 
 const server = app.listen(5000,() =>{
     console.log("Server is working")
